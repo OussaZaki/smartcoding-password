b3e0eee16746f4d33389150cfc1239c0cbd77a76
Natzsche <natallemoura7@gmail.com>
2018-07-11 00:28:53 +0200

    Add new generators for the password

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -12,16 +12,50 @@ function createPassword() {
     console.log(newPassword);
     return newPassword;
 }
-// where to return the generated password and how?
+
 // Math.floor = integer.
 // Math.random = generate an random float between 0 and 1, that's why we need the m.floor.
 // *26 = alphabet.
-function randomString(length) {
-    var newString = "";
+function randomLowerCase(length) {
+    var generatedLowerCase = "";
     var letters = "abcdefghijklmnopqrstuvwxyz";
     for (let i = 0; i < length; i++) {
         var randomNumber = Math.floor(Math.random()*26);
         newString += letters[randomNumber];
     }
-    return newString;
+    return generatedLowerCase;
+}
+
+function randomUpperCase(length) {
+    var generatedUpperCase = "";
+    var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (let i = 0; i < length; i++) {
+        var randomNumber = Math.floor(Math.random()*26);
+        newString += upperLetters[randomNumber];
+    }
+    return generatedUpperCase;
+}
+
+function randomNumber(length) {
+    var generatedNumber = "";
+    var numbers = "0123456789";
+    for (let i = 0; i < length; i++) {
+        var randomNumber = Math.floor(Math.random()*10);
+        newString += numbers[randomNumber];
+    }
+    return generatedNumber;
+}
+
+function randomSpecial(length) {
+    var generatedSpecial = "";
+    var special = "!@#$%^&*_-+=";
+    for (let i = 0; i < length; i++) {
+        var randomNumber = Math.floor(Math.random()*12);
+        newString += special[randomNumber];
+    }
+    return generatedSpecial;
+}
+
+function scramble() {
+
 }
