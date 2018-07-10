843d6be807909906eaf78f6fa262dbea2e2890f8
Fatemeh <40216419+FatemehEngqvist@users.noreply.github.com>
2018-07-10 19:04:13 +0200

    Update test.js

--- test/test.js
+++ test/test.js
@@ -12,3 +12,39 @@ describe('Utils |', function() {
     });
   });
 });
+
+describe('Utils |', function() {
+  describe('hasMixedCase', function() {
+    it('should return true when the string has a both upper and lower case letters', function() {
+      assert.equal(hasMixedCase("helloWorld"), true);
+    });
+
+    it('should return false when the string has no special char', function() {
+      assert.equal(hasMixedCase("helloworld"), false);
+    });
+  });
+});
+
+describe('Utils |', function() {
+  describe('hasValidLength', function() {
+    it('should return true when the string has the length more than 6', function() {
+      assert.equal(hasValidLength("helloworld"), true);
+    });
+
+    it('should return false when the string has no special char', function() {
+      assert.equal(hasValidLength("hello"), false);
+    });
+  });
+});
+
+describe('Utils |', function() {
+  describe('hasDigit', function() {
+    it('should return true when the string has the length more than 6', function() {
+      assert.equal(hasDigit("hello123"), true);
+    });
+
+    it('should return false when the string has no special char', function() {
+      assert.equal(hasDigit("hello"), false);
+    });
+  });
+});
