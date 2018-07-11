aff0c5fa8acd7904d2979bd62be845a024794b5e
KKsenija <ksenija.kabanova@hotmail.co.uk>
2018-07-11 17:50:08 +0200

    Added a partly functioning PIN code app

--- pinCode.js
+++ pinCode.js
@@ -4,7 +4,7 @@ digits.addEventListener('click', function()
 {
     buttonFunction();
 });
-
+// Work in progress! The function returns only 1 number!
 function buttonFunction(value)
 {
     document.getElementById("pinCode").value = document.getElementById('buttons').value;
