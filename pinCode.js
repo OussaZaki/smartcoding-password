4cfd0d71ada5ed68fadf6e924179bc48d6d6a669
KKsenija <ksenija.kabanova@hotmail.co.uk>
2018-07-11 18:08:16 +0200

    Added a partly functioning PIN code app

--- pinCode.js
+++ pinCode.js
@@ -9,3 +9,15 @@ function buttonFunction(value)
 {
     document.getElementById("pinCode").value = document.getElementById('buttons').value;
 }
+
+var clearInput = document.getElementById("clearIt");
+
+clearInput.addEventListener('click', function()
+{
+    clearFunction();
+});
+
+function clearFunction()
+{
+    document.getElementById("pinCode").value = "";
+}
