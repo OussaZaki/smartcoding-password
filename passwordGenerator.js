9e814f48ed243866512ca80dea9614d30c92eb06
Natzsche <natallemoura7@gmail.com>
2018-07-12 19:10:35 +0200

    Master function to randomize created

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -7,54 +7,29 @@ generate.addEventListener("click", function() {
 });
 
 function createPassword() {
-    var newPassword = randomLowerCase(3);
-    newPassword += randomUpperCase(3);
-    newPassword += randomNumber(1);
-    newPassword += randomSpecial(1);
+    var newPassword = randomize(letters, 3);
+    newPassword += randomize(upperLetters, 3);
+    newPassword += randomize(numbers, 1);
+    newPassword += randomize(special, 1);
     return scramble(newPassword);
 }
 
 // Math.floor = integer.
 // Math.random = generate an random float between 0 and 1, that's why we need the m.floor.
 // *26 = alphabet.
-function randomLowerCase(length) {
-    var generatedLowerCase = "";
-    var letters = "abcdefghijklmnopqrstuvwxyz";
-    for (let i = 0; i < length; i++) {
-        var randomNumber = Math.floor(Math.random()*26);
-        generatedLowerCase += letters[randomNumber];
-    }
-    return generatedLowerCase;
-}
 
-function randomUpperCase(length) {
-    var generatedUpperCase = "";
-    var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (let i = 0; i < length; i++) {
-        var randomNumber = Math.floor(Math.random()*26);
-        generatedUpperCase += upperLetters[randomNumber];
-    }
-    return generatedUpperCase;
-}
+var letters = "abcdefghijklmnopqrstuvwxyz";
+var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var special = "!@#$%^&*_-+=";
 
-function randomNumber(length) {
-    var generatedNumber = "";
-    var numbers = "0123456789";
+function randomize (stringToRandom, length) {
+    var randomGenerated = "";
     for (let i = 0; i < length; i++) {
-        var randomNumber = Math.floor(Math.random()*10);
-        generatedNumber += numbers[randomNumber];
+        var randomNumber = Math.floor(Math.random()*stringToRandom.length);
+        randomGenerated += stringToRandom[randomNumber];
     }
-    return generatedNumber;
-}
-
-function randomSpecial(length) {
-    var generatedSpecial = "";
-    var special = "!@#$%^&*_-+=";
-    for (let i = 0; i < length; i++) {
-        var randomNumber = Math.floor(Math.random()*12);
-        generatedSpecial += special[randomNumber];
-    }
-    return generatedSpecial;
+    return randomGenerated;
 }
 
 function scramble(stringToScramble) {
@@ -68,3 +43,43 @@ function scramble(stringToScramble) {
     var scrambled = stringToArray.join(""); // return to string.
     return scrambled;
 }
+
+// function randomLowerCase(length) {
+//     var generatedLowerCase = "";
+//     var letters = "abcdefghijklmnopqrstuvwxyz";
+//     for (let i = 0; i < length; i++) {
+//         var randomNumber = Math.floor(Math.random()*26);
+//         generatedLowerCase += letters[randomNumber];
+//     }
+//     return generatedLowerCase;
+// }
+
+// function randomUpperCase(length) {
+//     var generatedUpperCase = "";
+//     var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     for (let i = 0; i < length; i++) {
+//         var randomNumber = Math.floor(Math.random()*26);
+//         generatedUpperCase += upperLetters[randomNumber];
+//     }
+//     return generatedUpperCase;
+// }
+
+// function randomNumber(length) {
+//     var generatedNumber = "";
+//     var numbers = "0123456789";
+//     for (let i = 0; i < length; i++) {
+//         var randomNumber = Math.floor(Math.random()*10);
+//         generatedNumber += numbers[randomNumber];
+//     }
+//     return generatedNumber;
+// }
+
+// function randomSpecial(length) {
+//     var generatedSpecial = "";
+//     var special = "!@#$%^&*_-+=";
+//     for (let i = 0; i < length; i++) {
+//         var randomNumber = Math.floor(Math.random()*12);
+//         generatedSpecial += special[randomNumber];
+//     }
+//     return generatedSpecial;
+// }
